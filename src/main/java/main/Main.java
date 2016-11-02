d1ee8ac7bf424c08d83d2060ee5c0226dc77e573
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2016-11-02 18:58:49 +0300

    Add files: LICENCE, README, config file for log4j. Modified pom.xml file.

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by bbb1991 on 11/2/16.
+ *
+ * @author Bagdat Bimaganbetov
+ * @author bagdat.bimaganbetov@gmail.com
+ */
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("dgfsfgsfd");
+    }
+}
